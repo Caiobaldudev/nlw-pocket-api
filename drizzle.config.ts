1890f7157107bd8b3f75e93968c753a2cc153527
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-10 15:30:51 -0300

    feat: configuring base enviroment & zod i

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import {defineConfig} from 'drizzle-kit'
+import { env } from './src/env'
+
+
+export default defineConfig({
+  schema: "./schema.ts",
+  out: './.migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+})
