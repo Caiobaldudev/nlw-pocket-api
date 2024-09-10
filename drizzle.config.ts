a5bcbe8e87f88bef43105c46d6732758780d0e05
Caio Balduino <caiobalduinodev@hotmail.com>
2024-09-10 15:32:43 -0300

    feat: ready database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { env } from './src/env'
 
 
 export default defineConfig({
-  schema: "./schema.ts",
+  schema: "./src/db/schema.ts",
   out: './.migrations',
   dialect: 'postgresql',
   dbCredentials: {
